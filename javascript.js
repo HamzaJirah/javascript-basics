78586b8bb3124e24a46f77f080dd344f8bcafbc6
HamzaJirah <hamzajirah@gmail.com>
2018-02-09 00:49:38 +0100

    comment the first function

--- javascript.js
+++ javascript.js
@@ -127,7 +127,7 @@
 
 // FUNCTIONS(grouping all related statements together; while calling the name of the function to invoke the statements in it)
 
-function pearlClass (){
-  alert("you are Pearl Class")
-}
+// function pearlClass (){
+//   alert("you are Pearl Class")
+// }
 

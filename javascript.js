a4b3a6e8e8a4292fae2c9c58e323004c8da87ad8
HamzaJirah <hamzajirah@gmail.com>
2018-02-09 01:28:53 +0100

    invoked/called the function multiple times

--- javascript.js
+++ javascript.js
@@ -144,5 +144,9 @@
 // FUNCTION WITH MULTIPLE PARAMETERS
 function addNums(num1, num2){
   var answer = num1 + num2
-  document.write(answer)
+  document.write(answer + "<br>")
 }
+
+addNums(3, 5);
+addNums(5, 6);
+addNums(9,6);
